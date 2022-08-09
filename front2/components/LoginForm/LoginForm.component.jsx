344feb2abb9d75aafec00d89fb46c601a76e5df4
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-08-09 21:54:48 +0100

    add some form validation

--- front2/components/LoginForm/LoginForm.component.jsx
+++ front2/components/LoginForm/LoginForm.component.jsx
@@ -1,14 +1,41 @@
-import Input from '../Input/Input.component';
-import {Main , Button , ButContainer } from './LoginForm.styles';
+import {Main , Button , ButContainer , Label , InputBut , Wrap } from './LoginForm.styles';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 
 const LoginForm = () => {
   const sizeWidth=500;
 
+  const router = useRouter()
+
+
+
+  const [username, setUsername] = useState('');
+  const [password , setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username == "admin" && password == "admin"){
+          router.push("ads");
+    }
+    else{
+      alert('Incorrect Informations');
+      setUsername('');
+      setPassword('');
+
+    }
+  }
   return(
     <Main>
-      <form>
-        <Input label="Username" width={sizeWidth} type="text" />
-        <Input label="Password" width={sizeWidth} type="password" />
+      <form onSubmit={handleSubmit}>
+        <Wrap>
+          <Label>Username</Label>
+          <InputBut width={sizeWidth} type="text" value={username} onChange={(e) => setUsername(e.target.value) } />
+        </Wrap>
+        <Wrap>
+          <Label>Password</Label>
+          <InputBut width={sizeWidth} type="password" value={password}  onChange={(e) => setPassword(e.target.value) } />
+        </Wrap>
         <p>I forgot my password</p>
         <ButContainer>
           <Button>Sign in</Button>
--- front2/components/LoginForm/LoginForm.styles.js
+++ front2/components/LoginForm/LoginForm.styles.js
@@ -27,3 +27,20 @@ export const Button = styled.button`
     border : none;
   }
 `
+export const Label = styled.label`
+  color : #b5b3b3;
+  font-size : 15px;
+`
+
+export const InputBut = styled.input`
+  outline : none;
+  border : 1px solid #b5b3b3;
+  padding : 3px 6px;
+  width : ${({ width }) => `${width}px`};
+`
+export const Wrap = styled.div`
+  display : flex;
+  flex-direction : column;
+  margin-bottom : 10px;
+`
+
