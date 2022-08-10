0a5e974bf1667a4e59506b5834688ea895bbd623
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-08-11 00:01:33 +0100

    finish sales dashboard

--- front2/styles/PubDashboardPage.styles.js
+++ front2/styles/PubDashboardPage.styles.js
@@ -86,3 +86,68 @@ export const Flex = styled.div`
   height : 100px;
   margin-top : 40px;
 `
+
+export const Stats = styled.div`
+  display  : grid;
+  grid-template-columns: repeat(5,1fr);
+  gap : 10px;
+  margin-top : 30px;
+  height : 180px;
+
+`
+
+export const Item = styled.div`
+  background : #f2f2f2;
+  padding : 10px;
+  display : flex;
+  justify-content : center;
+  flex-direction : column;
+  align-items : center;
+`
+
+export const Final = styled.div`
+  display : grid;
+  grid-template-columns: repeat(2,1fr);
+  gap : 10px;
+  height : 250px;
+  margin-top : 50px;
+`
+
+export const Left = styled.div`
+  background : #f2f2f2;
+  display : flex;
+  flex-direction : column;
+  justify-content : space-between;
+  align-items : center;
+  padding : 5px 20px;
+`
+
+export const Right = styled.div`
+  background : #f2f2f2;
+  padding : 10px 20px;
+  display : flex;
+  flex-direction : column;
+  justify-content : space-between;
+  align-items : center;
+`
+
+export const Calendar = styled.div`
+  display : grid;
+  grid-template-columns: 2.2fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  height : 100px;
+  width : 100%;
+  gap : 1px;
+  padding-bottom : 15px;
+`
+
+export const Inside = styled.div`
+  border : 1px solid black;
+  font-size : 11px;
+  display : flex;
+  justify-content:center;
+  align-items :center;
+
+  &:first-of-type{
+    border : none;
+  }
+`
