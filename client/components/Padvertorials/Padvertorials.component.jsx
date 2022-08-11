c9f27530a0b6c5883a47757ec3afa92cc10d465f
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-08-11 16:39:03 +0100

    add advertorials component

--- client/components/Padvertorials/Padvertorials.component.jsx
+++ client/components/Padvertorials/Padvertorials.component.jsx
@@ -1,8 +1,77 @@
+import { Flex , Element , Double , Wrap , Label , InputBut , ButContainer , Button } from '../PdisplayAds/PdisplayAds.styles';
+import { useState } from 'react';
 
 
 const Padvertorials = () => {
+	const [name , setName] = useState('');
+	const [logo , setLogo] = useState('');
+	const [pageUrl , setPageUrl] = useState('');
+	const [dailyReadership , setDailyReadership] = useState('');
+
 	return(
 			<>
+				<Flex>
+					<Element>
+						<p style={{"fontWeight":"bold"}}>Details</p>
+						<Double style={{"marginTop":"25px"}}>
+				            <Wrap>
+					          <Label>Name</Label>
+					          <InputBut type="text" value={name} onChange={(e) => setName(e.target.value) } />
+					        </Wrap>
+					        <Wrap>
+					          <Label>Logo</Label>
+					          <InputBut type="text" value={logo} onChange={(e) =>  setLogo(e.target.value) } />
+					        </Wrap>
+				        </Double>
+
+				        <Double>
+				            <Wrap>
+					          <Label>Page URL</Label>
+					          <InputBut type="text" value={pageUrl} onChange={(e) => setPageUrl(e.target.value) } />
+					        </Wrap>
+					        <Wrap>
+					          <Label>Daily Readership</Label>
+					          <InputBut type="text" value={dailyReadership} onChange={(e) =>  setDailyReadership(e.target.value) } />
+					        </Wrap>
+				        </Double>
+					</Element>
+					<Element>
+						<p style={{"fontWeight":"bold"}}>Price List</p>
+						<table className="table">
+				          <thead style={{"padding":"0"}}>
+				            <tr style={{"color" : "#8f8f8f" , "height": "7px"}}>
+				              <th style={{"width":"63%" , "fontSize":"11px" }} scope="col">Options</th>
+				              <th style={{"width":"11%", "fontSize":"11px"}} scope="col">Measure</th>
+				              <th style={{"width":"13%", "fontSize":"11px"}} scope="col">Price (AED)</th>
+				              <th style={{"width":"13%", "fontSize":"11px"}} scope="col">Net (AED)</th>
+				            </tr>
+				          </thead>
+				          <tbody>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>(500 words + 1 Image)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>(500 words + 1 Image + 1 Backlink)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>(500 words + 1 Image + 1 Backlink + 1 Video)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				          </tbody>
+				        </table>
+					</Element>
+				</Flex>
+				<ButContainer>
+			          <Button>Save</Button>
+			    </ButContainer>
 			</>
 		);
 }
--- client/components/PsocialMedia/PsocialMedia.component.jsx
+++ client/components/PsocialMedia/PsocialMedia.component.jsx
@@ -1,8 +1,119 @@
+import { Flex , Element , Double , Wrap , Label , InputBut , ButContainer , Button } from '../PdisplayAds/PdisplayAds.styles';
+import { useState } from 'react';
 
 
 const PsocialMedia = () => {
+	const [name , setName] = useState('');
+	const [logo , setLogo] = useState('');
+	const [facebookPageUrl , setFacebookPageUrl] = useState('');
+	const [metaFollowers , setMetaFollowers] = useState('');
+	const [instagramPageURL, setInstagramPageURL] = useState('');
+	const [instagramFollowers, setInstagramFollowers] = useState('');
+
 	return(
 			<>
+				<Flex>
+					<Element>
+						<p style={{"fontWeight":"bold", "marginBottom":"5px"}}>Details</p>
+						<Double style={{"marginTop":"25px"}}>
+				            <Wrap>
+					          <Label>Name</Label>
+					          <InputBut type="text" value={name} onChange={(e) => setName(e.target.value) } />
+					        </Wrap>
+					        <Wrap>
+					          <Label>Logo</Label>
+					          <InputBut type="text" value={logo} onChange={(e) =>  setLogo(e.target.value) } />
+					        </Wrap>
+				        </Double>
+
+				        <Double>
+				            <Wrap>
+					          <Label>Facebook Page URL</Label>
+					          <InputBut type="text" value={facebookPageUrl} onChange={(e) => setFacebookPageUrl(e.target.value) } />
+					        </Wrap>
+					        <Wrap>
+					          <Label>Meta Followers</Label>
+					          <InputBut type="text" value={metaFollowers} onChange={(e) =>  setMetaFollowers(e.target.value) } />
+					        </Wrap>
+				        </Double>
+				        <Double>
+				        <Wrap>
+					          <Label>Instagram Page URL</Label>
+					          <InputBut type="text" value={instagramPageURL} onChange={(e) =>  setInstagramPageURL(e.target.value) } />
+					    </Wrap>
+					    <Wrap>
+					          <Label>Instagram Followers</Label>
+					          <InputBut type="text" value={instagramFollowers} onChange={(e) =>  setInstagramFollowers(e.target.value) } />
+					    </Wrap>
+					    </Double>
+					</Element>
+					<Element>
+						<p style={{"fontWeight":"bold"}}>Price List</p>
+						<table className="table">
+				          <thead style={{"padding":"0"}}>
+				            <tr style={{"color" : "#8f8f8f" , "height": "7px"}}>
+				              <th style={{"width":"65%" , "fontSize":"11px" }} scope="col">Options</th>
+				              <th style={{"width":"15%", "fontSize":"11px"}} scope="col">Measure</th>
+				              <th style={{"width":"10%", "fontSize":"11px"}} scope="col">Price (AED)</th>
+				              <th style={{"width":"10%", "fontSize":"11px"}} scope="col">Net (AED)</th>
+				            </tr>
+				          </thead>
+				          <tbody>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Meta Feed Image (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Meta Feed Video (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Meta Stories Image (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Meta Stories Video (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Instagram Feed Image (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Instagram Feed Video (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Instagram Stories Image (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				            <tr style={{"height":"4px"}}>
+				              <td style={{"padding":"2px 8px"}}>Instagram Stories Video (1,080 x 1,080 px)</td>
+				              <td style={{"padding":"2px 0"}}>Per Post</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				              <td style={{"padding":"2px 0"}}>0.00</td>
+				            </tr>
+				          </tbody>
+				        </table>
+					</Element>
+				</Flex>
+				<ButContainer>
+			          <Button>Save</Button>
+			    </ButContainer>
 			</>
 		);
 }
